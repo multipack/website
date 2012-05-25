a517c99f582e561110ee25d61f303737f56e1a65
Tom Ashworth <tom@phuu.net>
2012-05-25 15:27:50 +0100

    Bloody coulrophobics.

--- _multipack/view/error.php
+++ _multipack/view/error.php
@@ -7,7 +7,7 @@
     </div>
   </header>
   <article role="about">
-    <p>A crack team of coulrophobics has been dispatched to poke and prod until the problem is resolved.</p>
+    <p>A crack team of <a href="http://en.wikipedia.org/wiki/Coulrophobia">coulrophobics</a> has been dispatched to poke and prod until the problem is resolved.</p>
     <p>Try going <a href="<?=$this->url?>">home</a>.</p>
   </article>
 </section>

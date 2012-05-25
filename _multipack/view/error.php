dac416887f719f0f7171130e5216d12ca30f190e
Tom Ashworth <tom@phuu.net>
2012-05-25 14:40:41 +0100

    Make the error page nicerer.

--- _multipack/view/error.php
+++ _multipack/view/error.php
@@ -1,6 +1,13 @@
-  <article>
-    <h2>404.</h2>
-    <p>Error, no such page was found, sorry!</p>
+<section role="main">
+  <header>
+    <?php $this->view_component('nav'); ?>
+    <div class="branding">
+      <h1>404.</h1>
+      <h2>No such page was found, sorry!</h2>
+    </div>
+  </header>
+  <article role="about">
     <p>A crack team of coulrophobics has been dispatched to poke and prod until the problem is resolved.</p>
-    <p>Try going <a href=<?=$this->url?>>home</a>.</p>
+    <p>Try going <a href="<?=$this->url?>">home</a>.</p>
   </article>
+</section>

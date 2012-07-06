9b79fa270edecdfd5172a41340237ddb95c6eb50
Anthony Williams <anthony@abitgone.co.uk>
2012-07-06 09:45:56 +0100

    Implemented MapBox Static Maps API
    Completely eliminating Google Maps content from the site.

--- _multipack/view/event.php
+++ _multipack/view/event.php
@@ -8,11 +8,11 @@
 <p class="synopsis extra"><?=$view_component_data->tagline?></p>
 <div class="map iframe">
   <iframe width="425" height="350" frameBorder="0" src="http://a.tiles.mapbox.com/v3/multipack.map-vyj6asqh.html#16/<?=$view_component_data->venue['latitude']?>/<?=$view_component_data->venue['longitude']?>"></iframe>
-  <? /* ?><iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="<?=$view_component_data->map_url?>&amp;output=embed"></iframe><? */ ?>
 </div>
 
 <div class="map mapimage">
-  <a href="<?=$view_component_data->map_url?>"><img src="http://maps.google.com/maps/api/staticmap?center=,<?=$view_component_data->venue['longitude']?>&amp;zoom=15&amp;size=425x350&amp;sensor=false&amp;markers=size:mid|color:blue|<?=$view_component_data->venue['latitude']?>,<?=$view_component_data->venue['longitude']?>"></a>
+  <a href="<?=$view_component_data->map_url?>"><img src="http://a.tiles.mapbox.com/v3/multipack.map-vyj6asqh/pin-m-star+8aad36(<?=$view_component_data->venue['longitude']?>,<?=$view_component_data->venue['latitude']?>)/<?=$view_component_data->venue['longitude']?>,<?=$view_component_data->venue['latitude']?>,16/425x350.png"></a>
+  <p>Maps by <a href="http://mapbox.com/about/maps/">MapBox</a>, using <a href="http://www.openstreetmap.org/">OpenStreetMap</a> data, <a href="http://wiki.openstreetmap.org/wiki/OpenStreetMap_License">&copy; OpenStreetMap and contributors</a> and licensed <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>.</p>
 </div>
 <nav role="more">
   <ul>

41103de7ab1f59fdb58681806f5d68c28fc8bd10
Trevor <trovster@gmail.com>
2013-01-25 16:59:50 +0000

    Update Facebook URL

--- _multipack/view/footer.php
+++ _multipack/view/footer.php
@@ -21,7 +21,7 @@
           <h4>Also find us on&hellip;</h4>
           <ul>
             <li><a class="lanyrd" data-icon="l" href="//lanyrd.com/guides/multipack">Lanyrd</a></li>
-            <li><a class="facebook" data-icon="f" href="//www.facebook.com/group.php?gid=2230777424">Facebook</a></li>
+            <li><a class="facebook" data-icon="f" href="//www.facebook.com/groups/2230777424/">Facebook</a></li>
             <li><a class="flickr" data-icon="k" href="//flickr.com/groups/multipack/">Flickr</a></li>
             <li><a class="vimeo" data-icon="v" href="//vimeo.com/multipack/">Vimeo</a></li>
           </ul>

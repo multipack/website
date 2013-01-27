63a32e9403e428847c2fcb20f05a6e3f0da50288
Anthony Williams <anthony@abitgone.co.uk>
2013-01-27 17:20:29 +0000

    Show and Tell 2013 hiatus

--- _multipack/view/showandtell.php
+++ _multipack/view/showandtell.php
@@ -8,8 +8,8 @@
     </div>
   </header>
   <article role="about">
-    <p>Every three months, we put on a free event to give a forum to anyone — not just Multipack regulars — to showcase their talent and share their knowledge with the Midlands&rsquo; web community.</p>
-    <p>The Show and Tell format kicks off with an invited speaker, followed by 10 minute slots that are offered to new, emerging and seasoned speakers to engage with an enthusiastic audience of web and tech enthusiasts, not only from the Midlands and surrounding areas, but also the entire world via our live streaming webcasts.</p>
-    <p>More information on our next Show and Tell event will be shown here soon.</p>
+    <p><strong>We&rsquo;re taking a break this year, but we&rsquo;ll be back in 2014.</strong></p>
+    <p>Since 2009, we&rsquo;ve been putting on free events to give a forum to anyone &mdash; not just Multipack regulars &mdash; to showcase their talent and share their knowledge with the Midlands&rsquo; web community.</p>
+    <p>The Show and Tell format kicks off with an invited speaker, followed by 10-15 minute slots that are offered to new, emerging and seasoned speakers alike, giving anyone the chance to engage with an enthusiastic audience of web and tech enthusiasts, not only from the Midlands and surrounding areas, but also the entire world via our live streaming webcasts.</p>
   </article>
 </section>

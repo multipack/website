d860c60353804511a681ac559867c6044a615998
Trevor Morris <code@trovster.com>
2019-10-27 16:18:23 +0000

    fix: split out footer components

--- _multipack/view/footer.php
+++ _multipack/view/footer.php
@@ -1,30 +1,9 @@
     <footer class="main-footer">
       <section class="social">
         <h3>Stay connected and up-to-date:</h3>
-        <article class="tweets">
-          <a class="twitter-timeline" href="https://twitter.com/multipack" data-widget-id="519945419745267712" data-tweet-limit="3" data-chrome="nofooter transparent" >Tweets by @multipack</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-        </article>
-        <article class="social-media">
-          <h4>Also find us on…</h4>
-          <ul>
-            <li><a class="lanyrd" data-icon="l" href="//lanyrd.com/guides/multipack">Lanyrd</a></li>
-            <li><a class="facebook" data-icon="f" href="//www.facebook.com/groups/2230777424/">Facebook</a></li>
-            <li><a class="flickr" data-icon="k" href="//flickr.com/groups/multipack/">Flickr</a></li>
-            <li><a class="vimeo" data-icon="v" href="//vimeo.com/multipack/">Vimeo</a></li>
-          </ul>
-        </article>
-        <article class="newsletter">
-          <form action="http://multipack.createsend.com/t/y/s/akjr/" method="post" id="subForm">
-            <h4>Sign up for our newsletter:</h4>
-            <div class="newsletter-inputs">
-              <input type="text" name="cm-name" id="name" placeholder="Your Name" />
-              <input type="email" name="cm-akjr-akjr" id="akjr-akjr" placeholder="Your Email Address" />
-            </div>
-            <button type="submit">Sign Up</button>
-            <aside>No spam, or you can tell our mums.</aside>
-          </form>
-        </article>
+        <?php $this->view_component('tweets'); ?>
+        <?php $this->view_component('social_media'); ?>
+        <?php $this->view_component('newsletter'); ?>
       </section>
       <section class="block footer">
         <p class="copyright">&copy; The Multipack</p>
---
+++ _multipack/view/newsletter.php
@@ -0,0 +1,11 @@
+<article class="newsletter">
+  <form action="http://multipack.createsend.com/t/y/s/akjr/" method="post" id="subForm">
+    <h4>Sign up for our newsletter:</h4>
+    <div class="newsletter-inputs">
+      <input type="text" name="cm-name" id="name" placeholder="Your Name" />
+      <input type="email" name="cm-akjr-akjr" id="akjr-akjr" placeholder="Your Email Address" />
+    </div>
+    <button type="submit">Sign Up</button>
+    <aside>No spam, or you can tell our mums.</aside>
+  </form>
+</article>

8798a0e1d67554aebb0648b5a5de52388fe4f9cc
Trevor Morris <code@trovster.com>
2019-10-27 18:26:07 +0000

    fix: switch Lanyard for Twitter

--- _multipack/view/social_media.php
+++ _multipack/view/social_media.php
@@ -1,7 +1,7 @@
 <article class="social-media">
   <h4>Find us on…</h4>
   <ul>
-    <li><a class="lanyrd" href="//lanyrd.com/guides/multipack">Lanyrd</a></li>
+    <li><a class="twitter" href="//twitter.com/multipack">Twitter</a></li>
     <li><a class="facebook" href="//www.facebook.com/groups/2230777424/">Facebook</a></li>
     <li><a class="flickr" href="//flickr.com/groups/multipack/">Flickr</a></li>
     <li><a class="vimeo" href="//vimeo.com/multipack/">Vimeo</a></li>

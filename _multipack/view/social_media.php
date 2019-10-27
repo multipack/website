7eb536983cc747bf27894c8dedcc3e24526912fe
Trevor Morris <code@trovster.com>
2019-10-27 16:19:10 +0000

    fix: remove data icon from social media; missing font

--- _multipack/view/social_media.php
+++ _multipack/view/social_media.php
@@ -1,9 +1,9 @@
 <article class="social-media">
   <h4>Also find us on…</h4>
   <ul>
-    <li><a class="lanyrd" data-icon="l" href="//lanyrd.com/guides/multipack">Lanyrd</a></li>
-    <li><a class="facebook" data-icon="f" href="//www.facebook.com/groups/2230777424/">Facebook</a></li>
-    <li><a class="flickr" data-icon="k" href="//flickr.com/groups/multipack/">Flickr</a></li>
-    <li><a class="vimeo" data-icon="v" href="//vimeo.com/multipack/">Vimeo</a></li>
+    <li><a class="lanyrd" href="//lanyrd.com/guides/multipack">Lanyrd</a></li>
+    <li><a class="facebook" href="//www.facebook.com/groups/2230777424/">Facebook</a></li>
+    <li><a class="flickr" href="//flickr.com/groups/multipack/">Flickr</a></li>
+    <li><a class="vimeo" href="//vimeo.com/multipack/">Vimeo</a></li>
   </ul>
 </article>

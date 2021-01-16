d3e8c58ca4c8571356c4194bdd68cda3bdee25d8
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-01-16 14:32:57 +0000

    refactor: use javascript for all view data

---
+++ src/_data/navigation.js
@@ -0,0 +1,16 @@
+module.exports = [{
+  label: 'Home',
+  url: '/'
+}, {
+  label: 'Show and Tell',
+  url: '/show-and-tell/'
+}, {
+  label: 'Birmingham',
+  url: '/birmingham/'
+}, {
+  label: 'Leamington',
+  url: '/leamington/'
+}, {
+  label: 'London',
+  url: '/london/'
+}]
---
+++ src/_data/social-media.js
@@ -0,0 +1,13 @@
+module.exports = [{
+  label: 'Twitter',
+  url: '//twitter.com/multipack'
+}, {
+  label: 'Facebook',
+  url: '//www.facebook.com/groups/2230777424/'
+}, {
+  label: 'Flickr',
+  url: '//flickr.com/groups/multipack/'
+}, {
+  label: 'Vimeo',
+  url: '//vimeo.com/multipack'
+}]
